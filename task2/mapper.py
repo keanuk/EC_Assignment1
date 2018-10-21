e5a24aeff74fb29417901772ee42ab2ef5b116f0
Keanu Kerr <keanukerr@gmail.com>
2018-10-21 17:39:31 +0100

    Included sample datasets

--- task2/mapper.py
+++ task2/mapper.py
@@ -1,7 +1,6 @@
+#!/usr/bin/env python2
 import sys
 
-words = sys.stdin.read().split()
-
-for i in range(len(words) - 1):
+for i in range(len(sys.stdin.read().split()) - 1):
     print words[i] + "_" + words[i + 1], "\t", 1
     i += 1
