96de23589043bced77cbe3a0e117513609feb0cf
Keanu kerr <keanukerr@gmail.com>
2018-10-23 23:57:31 +0100

    Added skeleton files

---
+++ task8/run.sh
@@ -0,0 +1,18 @@
+OUTPUT_DIR=/user/${USER}/tasks/7
+OUTPUT_FILE=output.out
+
+hdfs dfs -rm -r $OUTPUT_DIR
+
+hadoop jar /opt/hadoop/hadoop-2.9.1/share/hadoop/tools/lib/hadoop-streaming-2.9.1.jar \
+  -D mapreduce.job.name=${USER}_task7 \
+  -input /data/small/imdb/title.basics.tsv \
+  -output $OUTPUT_DIR \
+  -mapper mapper.py \
+  -combiner combiner.py \
+  -reducer reducer.py \
+  -file mapper.py \
+  -file combiner.py \
+  -file reducer.py
+
+hdfs dfs -cat ${OUTPUT_DIR}/part-* | head -n 20 > $OUTPUT_FILE
+cat $OUTPUT_FILE
