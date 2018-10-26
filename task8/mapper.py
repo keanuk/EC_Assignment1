8ca9bc08149c56c7ceec6a8b964c1e659e9f2fc9
Keanu Kerr <keanukerr@gmail.com>
2018-10-26 15:12:15 +0100

    Task 8 progress

--- task8/mapper.py
+++ task8/mapper.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python2
 import sys
+
+for line in sys.stdin:
+    line = line.strip().split('\t')
+    if len(line) > 3:
+        if line[5] != '\N' and int(line[5]) >= 1900 and int(line[5]) <= 1999: 
+            print line[0], '\t', line[5]
+    else:
+        print line[0], '\t', line[1]
--- task8/reducer.py
+++ task8/reducer.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python2
 import sys
 
+prevKey = None
+prevVal = None
+
+for line in sys.stdin:
+    key, value = line.strip().split('\t')
+    if key == prevKey:
+        print prevVal, '\t', value
+    prevKey = key
+    prevVal = value
