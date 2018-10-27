1908fa05fe8c174033afcf6db2bf5876b5f55806
Keanu kerr <keanukerr@gmail.com>
2018-10-27 20:55:47 +0100

    Task 9

--- task9/mapper.py
+++ task9/mapper.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python2
 import sys
+
+for line in sys.stdin:
+    line = line.strip().split('\t')
+    if line[5] != '\N' and line[8] != '\N' and int(line[5]) >= 2000:
+        for genre in line[8].split(','):
+            print genre, '\t', 1
--- task9/reducer.py
+++ task9/reducer.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python2
 import sys
 
+count = 0
+prevGenre = None
+
+for line in sys.stdin:
+    genre, c = line.strip().split('\t')
+    if genre == prevGenre:
+        count += int(c)
+    elif prevGenre == None:
+        prevGenre = genre
+        count += int(c)
+    else:
+        print prevGenre, '\t', count
+        prevGenre = genre
+        count = int(c)
+print prevGenre, '\t', count
